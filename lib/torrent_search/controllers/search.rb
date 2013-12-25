9ff2271409a2b35427082c034d12f62040656c4c
Jon Neverland <jonwestin@gmail.com>
2013-12-25 02:55:45 +0100

    added spec for Search

--- lib/torrent_search/controllers/search.rb
+++ lib/torrent_search/controllers/search.rb
@@ -1,50 +1,51 @@
 module TorrentSearch
+  module Controllers
+    class Search
+      ACTIONS = {
+        d: :download,
+        s: :search,
+        q: :quit
+      }
 
-  ACTIONS = {
-    d: :download,
-    s: :search,
-    q: :quit
-  }
+      def initialize(view = Views::Search.new)
+        @view = view
+      end
 
-  class SearchController
-    def initialize(view = Views::Search.new)
-      @view = view
-    end
+      def search(search_terms = nil, options = {})
+        search_terms ||= @view.search_terms?
+        @search_result = Trackers::KickAss::Scraper.new(search_terms, options).search
+        Views::Menu.new(available_actions, @search_result).display
+        choose_action!
+        rescue SocketError
+          error 'No network connection?'
+      end
 
-    def search(search_terms = nil, options = {})
-      search_terms ||= @view.search_terms?
-      @search_result = Trackers::KickAss::Scraper.new(search_terms, options).search
-      Views::Menu.new(available_actions, @search_result).display
-      choose_action!
-      rescue SocketError
-        error 'No network connection?'
-    end
+      def quit
+        CLI::quit
+      end
 
-    def download
-      DownloadController.new(@search_result).download
-    end
+    private
+      def download
+        Download.new(@search_result).download
+      end
 
-    def quit
-      CLI::quit
-    end
+      def choose_action!
+        chosen = @view.action?
+        send available_actions.fetch(chosen, :invalid_command)
+      end
 
-  private
-    def choose_action!
-      chosen = @view.action?
-      send available_actions.fetch(chosen, :invalid_command)
-    end
+      def invalid_command
+        @view.invalid_command!
+        choose_action!
+      end
 
-    def invalid_command
-      @view.invalid_command!
-      choose_action!
-    end
+      def available_actions
+        ACTIONS.dup.tap {|hash| hash.delete(:d) unless results? }
+      end
 
-    def available_actions
-      ACTIONS.dup.tap {|hash| hash.delete(:d) unless results? }
-    end
-
-    def results?
-      @search_result.any?
+      def results?
+        @search_result.any?
+      end
     end
   end
 end
